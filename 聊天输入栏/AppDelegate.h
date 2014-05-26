d0e273321304aad5090c5793989a24e82db3ed6c
mahongwei <mahongwei@221.179.130.169>
2014-05-26 16:57:31 +0800

    Initial Commit

---
+++ 聊天输入栏/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  聊天输入栏
+//
+//  Created by mahongwei on 14-5-26.
+//  Copyright (c) 2014年 Fn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 聊天输入栏/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  聊天输入栏
+//
+//  Created by mahongwei on 14-5-26.
+//  Copyright (c) 2014年 Fn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
